2e8720f6908ec3fa883a0c842ab40cde05aa6789
MuharramOzodboyeva <muharramozodboyeva598@gmail.com>
2023-11-02 13:13:45 +0500

    changed

---
+++ lesson2.js
@@ -0,0 +1,160 @@
+//taks 2
+
+// boolean1
+// let A = 44;
+// let b = A / 2 == 0;
+// console.log(b);
+
+//boolean2
+// let A = -2;
+// let B = -1;
+// let C = A >= 0 || B < -2;
+// console.log(C);
+
+//boolean3
+// let a = 5;
+// let b = 6;
+// let c = 15;
+// let d = a < b && c > b;
+// console.log(d);
+
+//boolean4
+// let a = 10;
+// let b = 12;
+// let d = a % 2 !== 0 && b % 2 !== 0;
+// console.log(d);
+
+//boolean 5
+// let a = 5;
+// let b = 5;
+// let d = a % 2 == 0 || b % 2 !== 0 || b % 2 == 0 || a % 2 !== 0;
+// console.log(d);
+
+//boolean6 similar
+
+//boolean7
+// let a = 4;
+// let b = -2;
+// let c = 9;
+// let d = a > 0 && b > 0 && c > 0;
+// console.log(d);
+
+//boolean 8
+// let a = 4;
+// let b = -2;
+// let c = 0;
+// let d = a < 0 && c < 0 && (a > 0 || b > 0 || c > 0);
+// console.log(d);
+
+//boolean 9
+// let a = 4;
+// let b = -2;
+// let c = 0;
+// let d = a > 0 && c > 0 && (a > 0 || b > 0 || c > 0);
+// console.log(d);
+
+//boolean 10
+// let a = 12;
+// let b = 14;
+// let c = a % 2 == 0 && b % 2 == 0;
+// console.log(c);
+
+//boolean11
+// let a = 123;
+// let c = Math.trunc(a) ;
+// console.log(c);
+
+// Boolean10
+// let ab = 8;
+// let res = ab > 9 && ab % 2 == 0;
+// console.log(res);
+
+// Boolean11
+// let abc = -120;
+// let res = abc > 99 && abc % 2 == 1;
+// console.log(res);
+
+// Boolean12
+// let A = -1;
+// let B = 0;
+// let C = -5;
+// let res = A + B == 0 || A + C == 0 || B + C == 0;
+// console.log(res);
+
+// Boolean13
+// let abc = 359;
+// let bc = abc % 100;
+// let c = bc % 10;
+// let b = (bc - c) / 10;
+// let a = (abc - bc) / 100;
+// let d = a < b && b < c;
+// console.log(d);
+
+// Boolean14
+// let abc = 129;
+// let bc = abc % 100;
+// let c = bc % 10;
+// let b = (bc - c) / 10;
+// let a = (abc - bc) / 100;
+// let d = (a < b && b < c) || (a > b && b > c) ;
+// console.log(d);
+
+// Boolean15
+// let abc = 525;
+// let bc = abc % 100;
+// let c = bc % 10;
+// let b = (bc - c) / 10;
+// let a = (abc - bc) / 100;
+// let res = a == c && (b > a || b < a);
+// console.log(res);
+
+// Boolean16
+// let x1 = 4;
+// let y1 = 2;
+// let x2 = 1;
+// let y2 = 7;
+// let d = ((Math.abs(x2 - x1)) % 2 == 1 && (Math.abs(y2 - y1)) % 2 == 1);
+// console.log(d);
+
+// Boolean17
+// let x1 = 2;
+// let y1 = 5;
+// let x2 = 4;
+// let y2 = 8;
+// let d = (x1 == x2) || (y1 == 2);
+// console.log(d);
+
+// Boolean18
+// let x1 = 2;
+// let y1 = 4;
+// let x2 = 2;
+// let y2 = 3;
+// let d =
+//   (x1 == x2 || y2 == y1) && (Math.abs(x2 - x1) == 1 &&
+//     Math.abs(y2 - y1) == 1 &&
+//     Math.abs(x2 - x1) / Math.abs(y2 - y1) == 1);
+
+// console.log(d);
+
+// Operator1
+// let A = 2;
+// let B = 4;
+// let D = - ((B / 2) * (A ** 2 + B ** 2));
+// console.log(D);
+
+// Operator2
+// let A = 1;
+// let B = 3;
+// let C = 3;
+// let D = 5;
+// let Y = ((A + B) / (C + D)) ** 2 + B ** 2;
+// console.log(D);
+
+// Operator4
+
+// let A = 1;
+// let B = 2;
+// let C = 3;
+// let D = 4;
+// let E = (1 / A ** 2) * ((B / 10) ** 3) * (C + D) ** 2;
+// console.log(E);
